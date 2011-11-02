c350d9b9868c45d804f68c48d95ed069bcdf3179
shofetim <jordan@noionlabs.com>
2011-11-02 16:20:06 -0600

    Moved tests to new test file

--- tests/test.rkt
+++ tests/test.rkt
@@ -3,3 +3,16 @@
 (require rackunit
          "../main.rkt")
 
+;; Basic jexpr tests
+(check-true (jsexpr? #\null))
+(check-true (jsexpr? #t))
+(check-true (jsexpr? #f))
+(check-true (jsexpr? "I am string"))
+(check-true (jsexpr? 12))
+(check-true (jsexpr? -12))
+(check-true (jsexpr? -1.0))
+(check-true (jsexpr? 1E10))
+(check-true (jsexpr? '(1E10 #t "string")))
+(check-true (jsexpr? (hasheq 'foo "string")))
+(check-true (jsexpr? (hasheq 'foo 10)))
+(check-false (jsexpr? 3+4i))
